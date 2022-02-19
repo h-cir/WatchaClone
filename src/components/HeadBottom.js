c5d9c3447a2fff1c164d2fb12e6100b63f64a830
hovinee <hobinskyyy@naver.com>
2022-02-19 13:16:35 +0900

    디테일페이지 수정

--- src/components/HeadBottom.js
+++ src/components/HeadBottom.js
@@ -2,57 +2,153 @@ import React from "react";
 import styled from "styled-components";
 
 const HeaderBottom = () => {
-    return ( <Bottom>
-        <BottomOne>
-            <BottomContent>
-                <BottomContentOne>
-                    <BottomContentSectionOne>
-                        <div>
-
-                            <BottomContentSectionOneDiv1>
-                                <BottomContentSectionOneBasic>
+    return (
+        <Bottom>
+            <BottomInner>
+                <BottomContent>
+                    <BottomContentLeft>
+                        <div style={{ padding: "8px 0px 0px" }}>
+                            <div style={{ margin: "0px 20px" }}>
+                                <BottomContentBasicInfo>
                                     기본정보
-                                </BottomContentSectionOneBasic>
-                            </BottomContentSectionOneDiv1>
+                                </BottomContentBasicInfo>
+                            </div>
+                            <div>
+                                <div style={{ margin: "0px 20px" }}>
+                                    <BottomContentSectionOneArticle>
+                                        <BottomContentSectionOneArticleOne>
+                                            ドライブ・マイ・カー
+                                            <br />
+                                            <span>2021 · 일본 · 드라마</span>
+                                            <br />
+                                            <span>2시간 59분 · 15세</span>                                       '
+                                        </BottomContentSectionOneArticleOne>
+                                        <BottomContentSectionOneArticleTwo>
+                                            <BottomContentSectionOneArticleTwoDiv>
+                                                누가 봐도 아름다운 부부 가후쿠와 오토. 우연히 아내의 외도를 목격한 가후쿠는 이유를 묻지 못한 채 갑작스럽게 아내의 죽음을 맞이하게 된다. 2년 후 히로시마의 연극제에 초청되...
+                                            </BottomContentSectionOneArticleTwoDiv>
+                                        </BottomContentSectionOneArticleTwo>
+                                    </BottomContentSectionOneArticle>
+                                </div>
+                            </div>
+                            <hr style={{ border: "0", borderBottom: "1px solid #f0f0f0", margin: "24px 0 0" }} />
                         </div>
-                        <div>
 
-                            <BottomContentSectionOneDiv2>
-                                <BottomContentSectionOneArticle>
-                                    <BottomContentSectionOneArticleOne>
-                                        ドライブ・マイ・カー
-                                        <br />
-                                        <span>2021 · 일본 · 드라마</span>
-                                        <br />
-                                        <span>2시간 59분 · 15세</span>                                       </BottomContentSectionOneArticleOne>
-                                    <BottomContentSectionOneArticleTwo>
-                                        <BottomContentSectionOneArticleTwoDiv>
-                                        누가 봐도 아름다운 부부 가후쿠와 오토. 우연히 아내의 외도를 목격한 가후쿠는 이유를 묻지 못한 채 갑작스럽게 아내의 죽음을 맞이하게 된다. 2년 후 히로시마의 연극제에 초청되...
-                                        </BottomContentSectionOneArticleTwoDiv>
-                                    </BottomContentSectionOneArticleTwo>
-                                </BottomContentSectionOneArticle>
-                            </BottomContentSectionOneDiv2>
+
+                        <div style={{ padding: "8px 0px 0px" }}>
+                                <div style={{ margin: "0px 20px" }}>
+                                    <div style={{ overflow: "hidden" }}>
+                                        <BottomContentSectionTwocontent>
+                                            출연/제작
+                                        </BottomContentSectionTwocontent>
+                                    </div>
+                                </div>
+                            <BottomContentSectionTwoDiv2>
+                                <div style={{ overflow: "hidden" }}>
+                                    <div style={{ margin: "0 15px 0 25px" }}>
+                                        <BottomContentSectionTwoActorBox>
+                                            <BottomContentSectionTwoActor>
+                                                <BottomContentSectionTwoActor2>
+                                                    루벤 플레이셔 (감독)
+                                                </BottomContentSectionTwoActor2>
+                                            </BottomContentSectionTwoActor>
+
+
+                                            <BottomContentSectionTwoActor>
+                                                <BottomContentSectionTwoActor2>
+                                                    루벤 플레이셔 (감독)
+                                                </BottomContentSectionTwoActor2>
+                                            </BottomContentSectionTwoActor>
+
+                                            <BottomContentSectionTwoActor>
+                                                <BottomContentSectionTwoActor2>
+                                                    루벤 플레이셔 (감독)
+                                                </BottomContentSectionTwoActor2>
+                                            </BottomContentSectionTwoActor>
+
+
+                                        </BottomContentSectionTwoActorBox>
+                                    </div>
+                                </div>
+                            </BottomContentSectionTwoDiv2>
+                            <BottomContentSectionTwoDiv3>
+                                <hr style={{ border: "0", borderBottom: "1px solid #f0f0f0", margin: "24px 0 0" }} />
+                            </BottomContentSectionTwoDiv3>
+
+
                         </div>
-                    </BottomContentSectionOne>
 
-                    <BottomContentSectionTwo>출연/제작</BottomContentSectionTwo>
-                    <BottomContentSectionThree>별점 그래프</BottomContentSectionThree>
-                    <BottomContentSectionFour>코멘트</BottomContentSectionFour>
-                </BottomContentOne>
-            </BottomContent>
+                        <div style={{ padding: "8px 0px 0px" }}>
+                            <div>
+                                <div style={{ margin: "0 20px", overflow: "hidden" }}>
+                                    <h2 style={{ float: "left", fontSize: "19px", fontWeight: "700", letterSpacing: "0,7px", lineHeight: "28px", margin: "8px 0" }}>
+                                        별점그래프
+                                    </h2>
+                                    <span style={{ float: "right", color: "#282828", fontSize: "17px", fontWeight: "400", letterSpacing: "0,7px", lineHeight: "22px", textAlign: "right", margin: "10px 0 0" }}>
+                                        평균★2.8
+                                        <br />
+                                        <strong style={{ fontSize: "14px", fontWeight: "400", letterSpacing: "-0.3px", lineHeight: "19px", color: "#787878" }}>
+                                            (1,568명)
+                                        </strong>
+                                    </span>
+                                </div>
+                            </div>
+                            <div>
+                                <div style={{ margin: "0 20px" }}>
+                                    <div style={{ maxWidth: "375px", margin: "58px auto 0" }}></div>
+                                </div>
+                                <hr style={{ border: "0", borderBottom: "1px solid #f0f0f0", margin: "24px 0 0" }} />
+                            </div>
+                        </div>
+                        <div style={{ padding: "8px 0px 0px" }}>
+                            <div style={{ margin: "0 20px" }}>
+                                <header style={{ overflow: "hidden" }}>
+                                    <h2 style={{ float: "left", color: "#000", fontSize: "19px", fontWeight: "700", letterSpacing: "-0.7px", lineHeight: "28px", margin: "8px 0" }}>
+                                        코멘트
+                                    </h2>
+                                    <span style={{ display: "inline-block", color: "#a0a0a0", fontSize: "15px", fonwWeight: "400", letterSpacing: "-0.5px", lineHeight: "20px", margin: "12px 0 12px 6px" }}>
+                                        +80
+                                    </span>
+                                </header>
+                            </div>
+                            <div style={{ overflow: "hidden", padding: "0 5px", margin: "0 -5px" }}>
+                                <div style={{ margin: "0 20px" }}>
+                                    <ul style={{ listStyle: "none", padding: "0px", margin: "0px", whiteSpace: "nowrap", marginRight: "-5px !important", marginLeft: "-5px !important", marginTop: "14px", marginBottom: "30px" }}>
+                                        <li style={{ width: "50%", display: "inline-block", verticalAlign: "top", boxSizing: "border-box", padding: "0 5px" }}>
+                                            <div style={{ backgroundColor: "#f2f2f2", boxSizing: "border-box", padding: "0 12px", borderRadius: "6px", overflow: "hidden" }}>
+                                                <div style={{ display: "flex", borderBottom: "1px solid #e5e5e5", overflow: "hidden" }}>
+                                                    <div style={{ flex: "1", fontSize: "17px", fontWeight: "400", letterSpacing: "-0.7px", lineHeight: "22px", overflow: "hidden" }}>
+                                                        <a style={{ dispay: "flex", flex: "1", alignItems: "center", color: "#000" }}>유저닉네임</a>
+                                                    </div>
+                                                    <div></div>
+                                                </div>
+                                                <div>코멘트내용</div>
+                                                <div>좋아요 이모티콘</div>
+                                                <div>좋아요버튼</div>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+
+
+                            </div>
+                        </div>
+                    </BottomContentLeft>
+                </BottomContent>
 
 
 
-            <BottomSide>
-            <BottomSideTwo>
-            <div>
-            <BottomGallery>사진</BottomGallery>
-            <BottomVideo>비디오</BottomVideo>
-            </div>
-            </BottomSideTwo>
-            </BottomSide>
-        </BottomOne>
-    </Bottom>)
+                <BottomSide>
+                    <BottomSideTwo>
+                     
+                            <BottomGallery>사진</BottomGallery>
+                            <BottomVideo>비디오</BottomVideo>
+                      
+                    </BottomSideTwo>
+                </BottomSide>
+            </BottomInner>
+        </Bottom>
+    )
 }
 
 const Bottom = styled.div`
@@ -60,38 +156,33 @@ padding: 28px 0px 48px;
 overflow: hidden;
 display: block;
 `
-const BottomOne = styled.div`
+const BottomInner = styled.div`
 max-width: 976px;
 margin: 0px auto;
-display: block;
+display: table;
+
 `
 const BottomContent = styled.div`
-float: left;
 width: 640px;
 padding: 0px 8px;
-display: block;
+display: table-cell
 `
-const BottomContentOne = styled.div`
+const BottomContentLeft = styled.div`
 border-right: 1px solid;
-    border-left: 1px solid;
-    border-top: 1px solid;
-    border-top-left-radius: 6px;
-    border-top-right-radius: 6px;
-    background: rgb(255, 255, 255);
-    overflow: hidden;
-    border-color: rgb(227, 227, 227) !important;
-    display: block;
-`
-const BottomContentSectionOne = styled.div`
-padding: 8px 0px 0px;
+border-left: 1px solid;
+border-top: 1px solid;
+border-bottom: 1px solid;
+border-top-left-radius: 6px;
+border-top-right-radius: 6px;
+border-bottom-left-radius: 6px;
+border-bottom-right-radius: 6px;
+background: rgb(255, 255, 255);
+overflow: hidden;
+border-color: rgb(227, 227, 227) !important;
 display: block;
 `
-const BottomContentSectionOneDiv1 = styled.div`
-margin: 0px 20px;
-display: block;
-`
-const BottomContentSectionOneBasic = styled.div`
 
+const BottomContentBasicInfo = styled.div`
 color: rgb(0, 0, 0);
 font-size: 19px;
 font-weight: 700;
@@ -103,17 +194,14 @@ margin-block-end: 0.83em;
 margin-inline-start: 0px;
 margin-inline-end: 0px;
 `
-const BottomContentSectionOneDiv2 = styled.div`
-margin: 0px 20px;
-display: block;
-`
+
 const BottomContentSectionOneArticle = styled.div`
 font-size: 15px;
 font-weight: 400;
 letter-spacing: -0.5px;
 line-height: 20px;
 color: rgb(74, 74, 74);
-display: block;
+
 `
 const BottomContentSectionOneArticleOne = styled.div`
 color: rgb(74, 74, 74);
@@ -122,7 +210,7 @@ font-weight: 400;
 letter-spacing: -0.2px;
 line-height: 24px;
 margin: 8px 0px;
-display: block;
+
 `
 const BottomContentSectionOneArticleTwo = styled.div`
 word-break: break-all;
@@ -134,7 +222,7 @@ font-weight: 400;
 letter-spacing: -0.5px;
 line-height: 20px;
 color: rgb(74, 74, 74);
-display: block;
+
 `
 const BottomContentSectionOneArticleTwoDiv = styled.div`
 white-space: pre-wrap;
@@ -145,24 +233,56 @@ letter-spacing: -0.2px;
 line-height: 24px;
 `
 
-const BottomContentSectionTwo = styled.div`
-padding: 8px 0px 0px;
-display: block;
+const BottomContentSectionTwoDiv2 = styled.div`
+position: relative;
+line-height: normal;
+transform: translate3d(0,0,0);
+font-family: RobotoInCjk, "Noto Sans KR", "Apple SD Gothic Neo", "Nanum Gothic", "Malgun Gothic", sans-serif;
+`
+const BottomContentSectionTwocontent = styled.h2`
+float: left;
+color: #000;
+font-size: 19px;
+font-weight: 700;
+letter-spacing: -0.7px;
+line-height: 28px;
+margin: 8px 0;
 `
 
-const BottomContentSectionThree = styled.div`
-padding: 8px 0px 0px;
-display: block;
+const BottomContentSectionTwoActorBox = styled.div`
+list-style: none;
+padding: 0;
+margin: 0;
+height: 228px;
+display: flex;
+align-content: flex-start;
+margin-right: -5px;
+margin-left: -5px;
+margin-top: 4px;
+margin-bottom: 16px;
 `
-const BottomContentSectionFour = styled.div`
-padding: 8px 0px 0px;
-display: block;
+const BottomContentSectionTwoActor = styled.li`
+width: 50%;
+box-sizing: border-box;
+padding-right: 15px;
 `
+
+const BottomContentSectionTwoActor2 = styled.a`
+align-items: center;
+color: inherit;
+text-decoration: none;
+height: 76px;
+background-color: transparent;
+`
+
+const BottomContentSectionTwoDiv3 = styled.div`
+line-height: normal;
+`
+
 const BottomSide = styled.div`
-float: right;
 width: 320px;
 padding: 0px 8px;
-display: block;
+display: table-cell
 `
 const BottomSideTwo = styled.div`
 border: 1px solid;
@@ -170,7 +290,6 @@ border-radius: 6px;
 background: rgb(255, 255, 255);
 overflow: hidden;
 border-color: rgb(227, 227, 227) !important;
-display: block;
 `
 const BottomGallery = styled.div`
 padding: 8px 0px 0px;

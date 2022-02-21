8a80109c51a8b512ab8685c838dd0ee06fe61f49
Jeong Hye in <cukimonster725@naver.com>
2022-02-21 18:55:55 +0900

    NonUser모달 , signup모달 수정

--- src/components/NonUserModal.js
+++ src/components/NonUserModal.js
@@ -43,11 +43,43 @@ const NouUserModal = () => {
         <Box sx={style}>
           <div style={{ padding: "15px 0px 48px" }}>
             <section style={{ margin: "0 20px" }}>
-            <div class="css-l84u8"><span src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij4KICAgIDxwYXRoIGZpbGw9IiM3ODc4NzgiIGZpbGwtcnVsZT0iZXZlbm9kZCIgZD0iTTkuODU3IDE3Ljc4Nkw2IDIxdi00LjkxYy0xLjg0MS0xLjM3My0zLTMuMzY5LTMtNS41OUMzIDYuMzU4IDcuMDMgMyAxMiAzczkgMy4zNTggOSA3LjVjMCA0LjE0Mi00LjAzIDcuNS05IDcuNS0uNzM5IDAtMS40NTYtLjA3NC0yLjE0My0uMjE0eiIvPgo8L3N2Zz4K" class="css-1hj09ha"></span></div>
+              <div style={{    display: "flex",
+    flex: "1 1 0%",
+    justifyContent: "center", 
+    alignItems: "center",}}>
+                <span
+                  src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij4KICAgIDxwYXRoIGZpbGw9IiM3ODc4NzgiIGZpbGwtcnVsZT0iZXZlbm9kZCIgZD0iTTkuODU3IDE3Ljc4Nkw2IDIxdi00LjkxYy0xLjg0MS0xLjM3My0zLTMuMzY5LTMtNS41OUMzIDYuMzU4IDcuMDMgMyAxMiAzczkgMy4zNTggOSA3LjVjMCA0LjE0Mi00LjAzIDcuNS05IDcuNS0uNzM5IDAtMS40NTYtLjA3NC0yLjE0My0uMjE0eiIvPgo8L3N2Zz4K"
+                  class="css-1hj09ha"
+                ></span>
+                <div style={{width:"100%", textAlign:"center",padding:"50px 0px",margin:"0px 20px"}}>
+                <div
+                  style={{
+                    display: "inline-block",
+                    width: "88px",
+                    height: "88px",
+                    margin: "0 0 47px",
+                    background:
+                      "url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij4KICAgIDxwYXRoIGZpbGw9IiM3ODc4NzgiIGZpbGwtcnVsZT0iZXZlbm9kZCIgZD0iTTkuODU3IDE3Ljc4Nkw2IDIxdi00LjkxYy0xLjg0MS0xLjM3My0zLTMuMzY5LTMtNS41OUMzIDYuMzU4IDcuMDMgMyAxMiAzczkgMy4zNTggOSA3LjVjMCA0LjE0Mi00LjAzIDcuNS05IDcuNS0uNzM5IDAtMS40NTYtLjA3NC0yLjE0My0uMjE0eiIvPgo8L3N2Zz4K) center center / contain no-repeat",
+                  }}
+                ></div>
+                <p
+                  style={{
+                    fontSize: "19px",
+                    fontWeight: "700",
+                    letterSpacing: "-0.7px",
+                    lineHeight: "30px",
+                    whiteSpace: "pre-line",
+                    textAlign: "center",
+                    margin:"0px"
+                  }}
+                >
+                  지금 회원가입 혹은 로그인하시고, 경험한 감동을 기록해보세요.
+                </p>
+                </div>
+              </div>
               <SignupButton>회원가입</SignupButton>
               <LoginButton>로그인</LoginButton>
-              <Loginmodal />
-              <Signupmodal />
+              {/* <Loginmodal /><Signupmodal /> */}
             </section>
           </div>
         </Box>
@@ -102,7 +134,7 @@ const SignupButton = styled.button`
   width: 100%;
   height: 44px;
   border-radius: 6px;
-  margin: 16px 0px 0px;
+  margin: 16px 3px 0px;
 `;
 
 export default NouUserModal;
